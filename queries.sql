0cd3da01fef4cc54d43ce3144598b6886eee65fb
vy3191 <venkyafhs@gmail.com>
2019-12-18 21:23:52 -0500

    initial commit and answering queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,21 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * 
+FROM Customers
+WHERE PostalCode = 1010
+LIMIT 3;
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone
+FROM Suppliers
+WHERE SupplierID = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT *
+FROM Orders
+ORDER BY OrderDate DESC
+LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 
