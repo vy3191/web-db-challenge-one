b1e4624958c38f25ac534b4b539ae600111a2140
vy3191 <venkyafhs@gmail.com>
2019-12-19 19:33:25 -0500

    Added get method for accounts

--- server.js
+++ server.js
@@ -9,17 +9,19 @@ server.get("/", (req,res) => {
     res.json({msg:`working now`});
 });
 
-server.get("/api/accounts", (req,res) => {
-    try {
-
-    }catch(err) {
-       res.status(500).json({msg:`something went wrong with server`});
-    }
+server.get("/api/accounts", async (req,res) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+    res.status(200).json(accounts)
+ }catch(err) {
+    res.status(500).json({msg:`something went wrong with server`});
+ }
 });
 
-server.get("/api/accunts/:id", (req,res) => {
+server.get("/api/accounts/:id", async (req,res) => {
   try {
-
+     const accounts = db.select("*").from("accounts");
+     res.status(200).json(accounts)
   }catch(err) {
      res.status(500).json({msg:`something went wrong with server`});
   }
