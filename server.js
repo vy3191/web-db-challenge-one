83d0a8ef6c5912c7d469b58e837876d788bb1050
vy3191 <venkyafhs@gmail.com>
2019-12-19 19:22:02 -0500

    Added crud templates

--- server.js
+++ server.js
@@ -5,5 +5,48 @@ const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+server.get("/", (req,res) => {
+    res.json({msg:`working now`});
+});
+
+server.get("/api/accounts", (req,res) => {
+    try {
+
+    }catch(err) {
+       res.status(500).json({msg:`something went wrong with server`});
+    }
+});
+
+server.get("/api/accunts/:id", (req,res) => {
+  try {
+
+  }catch(err) {
+     res.status(500).json({msg:`something went wrong with server`});
+  }
+});
+
+server.post("/api/accounts", (req,res) => {
+  try {
+
+  }catch(err) {
+     res.status(500).json({msg:`something went wrong with server`});
+  }
+});
+
+server.put("/api/accunts/:id", (req,res) => {
+  try {
+
+  }catch(err) {
+     res.status(500).json({msg:`something went wrong with server`});
+  }
+});
+
+server.delete("/api/accunts/:id", (req,res) => {
+  try {
+
+  }catch(err) {
+     res.status(500).json({msg:`something went wrong with server`});
+  }
+});
 
 module.exports = server;
